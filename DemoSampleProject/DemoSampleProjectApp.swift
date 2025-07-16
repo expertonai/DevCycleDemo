859d284b56017a5ed9c4bd78c045161427f2e562
Varun Nagpal <emailvarunnagpal@gmail.com>
2025-07-16 11:07:40 -0400

    Initial Commit

---
+++ DemoSampleProject/DemoSampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSampleProjectApp.swift
+//  DemoSampleProject
+//
+//  Created by Varun Nagpal on 2025-07-16.
+//
+
+import SwiftUI
+
+@main
+struct DemoSampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoSampleProjectTests/DemoSampleProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSampleProjectTests.swift
+//  DemoSampleProjectTests
+//
+//  Created by Varun Nagpal on 2025-07-16.
+//
+
+import Testing
+@testable import DemoSampleProject
+
+struct DemoSampleProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
